d6025770bcfeb5371fbc7d29b7d46bade8fe6238
Necktrox <necktrox@gmail.com>
2016-08-03 01:02:45 +0200

    Update util table.lua
    Added table.values
    Added table.empty
    Added table.eachtry
    Added table.unpack
    Added table.destroy
    Added table.raw
    Added table.rawcopy
    Added table.json
    Improved table.size
    Improved table.random
    Improved table.pack
    Improved table.reverse
    Modified table.map
    Modified table.merge
    Renamed table.merge_insert to table.insert
    Renamed table.removevalue to table.skipvalue
    Renamed table.deletevalue to table.unsetvalue

--- shared-util/table.lua
+++ shared-util/table.lua
@@ -1,9 +1,4 @@
 
-local table = table
-local assert = assert
-local pairs = pairs
-local type = type
-
 function table.flip(self)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
 
@@ -33,16 +28,37 @@ function table.keys(self)
     return keys
 end
 
+function table.values(self)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+
+    local values = {}
+    local index = 1
+
+    for key, value in pairs(self) do
+        values[index] = value
+        index = index + 1
+    end
+
+    return values
+end
+
 function table.size(self)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
 
+    local index = next(self)
     local size = 0
 
-    for _ in pairs(self) do
+    while index ~= nil do
         size = size + 1
+        index = next(self, index)
     end
 
     return size
+end
+
+function table.empty(self)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+    return next(self) == nil
 end
 
 function table.map(self, callback, ...)
@@ -68,7 +84,7 @@ function table.maptry(self, callback, ...)
         end
     end
 
-    return self
+    return true
 end
 
 function table.each(self, callback)
@@ -82,12 +98,26 @@ function table.each(self, callback)
     return self
 end
 
-function table.merge(self, other)
+function table.eachtry(self, callback)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+    assert(type(callback) == "function", "expected function at argument 2, got ".. type(callback))
+
+    for key, value in pairs(self) do
+        if not callback(key, value) then
+            return false
+        end
+    end
+
+    return true
+end
+
+function table.merge(self, other, overwrite)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
     assert(type(other) == "table", "expected table at argument 2, got ".. type(other))
+    assert(overwrite == nil or type(overwrite) == "boolean", "expected boolean at argument 3, got ".. type(overwrite))
 
     for key, value in pairs(other) do
-        if self[key] == nil then
+        if overwrite or self[key] == nil then
             self[key] = value
         end
     end
@@ -95,7 +125,7 @@ function table.merge(self, other)
     return self
 end
 
-function table.merge_insert(self, other)
+function table.insert(self, other)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
     assert(type(other) == "table", "expected table at argument 2, got ".. type(other))
 
@@ -108,7 +138,7 @@ function table.merge_insert(self, other)
     return self
 end
 
-function table.removevalue(self, value)
+function table.skipvalue(self, value)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
     assert(value ~= nil, "expected any value at argument 2, got nil")
 
@@ -121,7 +151,6 @@ function table.removevalue(self, value)
             self[index - 1] = self[index]
         else
             if self[index] == value then
-                self[index] = nil
                 found = index
                 collapse = true
             end
@@ -135,7 +164,7 @@ function table.removevalue(self, value)
     return found
 end
 
-function table.deletevalue(self, value)
+function table.unsetvalue(self, value)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
     assert(value ~= nil, "expected any value at argument 2, got nil")
 
@@ -151,7 +180,8 @@ end
 
 function table.random(self)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
-    return self[math.random(#self)]
+    local length = #self
+    return length > 0 and self[math.random(#self)] or nil
 end
 
 function table.copy(self)
@@ -201,8 +231,8 @@ function table.pack(self)
     local length = 0
 
     for key, value in pairs(self) do
-        packed[length] = { key, value }
         length = length + 1
+        packed[length] = { key, value }
         self[key] = nil
     end
 
@@ -213,14 +243,145 @@ function table.pack(self)
     return self
 end
 
+function table.unpack(self)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+
+    local length = #self
+
+    if length == 0 then
+        return self
+    end
+
+    local unpacked = {}
+
+    for index = 1, length do
+        unpacked[self[index][1]] = self[index][2]
+        self[index] = nil
+    end
+
+    for key, value in pairs(unpacked) do
+        self[key] = value
+    end
+
+    return self
+end
+
 function table.reverse(self)
     assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
 
     local length = #self
 
-    for index = 1, length do
-        self[index] = self[length - (index - 1)]
+    if length < 2 then
+        return self
+    end
+
+    for index = 1, length / 2 do
+        local other = length - (index - 1)
+        self[index], self[other] = self[other], self[index]
     end
 
     return self
+end
+
+function table.destroy(self, deepdestroy)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+    assert(deepdestroy == nil or type(deepdestroy) == "boolean", "expected boolean at argument 2, got ".. type(deepdestroy))
+
+    local keys = {}
+    local index = 0
+
+    for key in pairs(self) do
+        if deepdestroy or type(self[key]) ~= "table" then
+            index = index + 1
+            keys[index] = key
+        end
+    end
+
+    if index == 0 then
+        return self
+    end
+
+    for index, key in pairs(keys) do
+        self[key] = nil
+    end
+
+    return self
+end
+
+local function isKeyValuePairJSONCompatible(key_t, value_t)
+    return (key_t == "boolean" or key_t == "number" or key_t == "string")
+        and (value_t == "boolean" or value_t == "number" or value_t == "string" or value_t == "table") 
+end
+
+function table.raw(self)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+
+    local references = {}
+
+    local function recursive(self)
+        for key, value in pairs(self) do
+            local key_t = type(key)
+            local value_t = type(value)
+
+            if isKeyValuePairJSONCompatible(key_t, value_t) then
+                if value_t == "table" then
+                    -- Recursive
+                    if references[key] == nil then
+                        references[key] = recursive(value)
+                    end
+
+                    self[key] = references[key]
+                else
+                    self[key] = value
+                end
+            else
+                self[key] = nil
+            end
+        end
+
+        return self
+    end
+
+    return recursive(self)
+end
+
+function table.rawcopy(self)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+
+    local references = {}
+
+    local function recursive(self)
+        local copy = {}
+
+        for key, value in pairs(self) do
+            local key_t = type(key)
+            local value_t = type(value)
+
+            if isKeyValuePairJSONCompatible(key_t, value_t) then
+                if value_t == "table" then
+                    -- Recursive
+                    if references[key] == nil then
+                        references[key] = recursive(value)
+                    end
+
+                    copy[key] = references[key]
+                else
+                    copy[key] = value
+                end
+            end
+        end
+
+        return copy
+    end
+
+    return recursive(self)
+end
+
+function table.json(self, compact)
+    assert(type(self) == "table", "expected table at argument 1, got ".. type(self))
+    assert(compact == nil or type(compact) == "boolean", "expected boolean at argument 2, got ".. type(compact))
+    -- 1) Create a copy of the table with discarded JSON-incompatible properties
+    -- 2) Convert this copy of table to a JSON string
+    -- 3) Remove the array brackets around the JSON object (and the spaces when compact is off)
+    return toJSON(table.rawcopy(self), compact):sub(compact and 2 or 3, compact and -2 or -3)
 end
